2b04d43724c28c439aeac24b89f7595a86feaabc
Rajkumar Sawant <58840962+codeplusmath@users.noreply.github.com>
2020-10-16 17:56:26 +0530

    Add files via upload

---
+++ sqaure.py
@@ -0,0 +1,13 @@
+#python program to create
+
+import turtle
+t=turtle.Turtle()
+turtle.bgcolor("black")
+turtle.pencolor("green")
+turtle.pensize(10)
+
+for i in range(4):
+	turtle.forward(100)
+	turtle.right(90)
+
+turtle.done()
